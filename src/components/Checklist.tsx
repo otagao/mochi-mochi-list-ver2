e54d2633262ffe07bc0455242f40a40bae959d00
goat-horn <massu141229@gmail.com>
2025-09-18 15:24:24 +0900

    チェックをつけたものは下に行くように修正

--- src/components/Checklist.tsx
+++ src/components/Checklist.tsx
@@ -33,6 +33,12 @@ export default function Checklist() {
     ])
   }
 
+  //  ソート
+  const sortedItems = [...items].sort((a, b) => {
+    return Number(a.isChecked) - Number(b.isChecked)
+  })
+
+
   return (
     <div className="max-w-md mx-auto p-4 bg-white rounded shadow">
       {/* タイトル */}
@@ -43,7 +49,7 @@ export default function Checklist() {
 
       {/* チェックリスト */}
       <ul className="space-y-2">
-        {items.map(item => (
+        {sortedItems.map(item => (
           <ChecklistItem
             key={item.id}
             item={item}
